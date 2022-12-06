95a0a50184a03cfb7a5e021ab3f869f947bdc828
ivan.bilokon <ivan.bilokon@jetsoftpro.com>
2022-12-06 22:26:05 +0200

    Updates

---
+++ precache-manifest.cd28e2dafa47010dc4acfd9badbd85fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e149a65cbb22926919dc",
+    "url": "/joker/static/css/main.5f8e843c.chunk.css"
+  },
+  {
+    "revision": "e149a65cbb22926919dc",
+    "url": "/joker/static/js/main.e149a65c.chunk.js"
+  },
+  {
+    "revision": "fbee9149cc341e4ff9a0",
+    "url": "/joker/static/js/1.fbee9149.chunk.js"
+  },
+  {
+    "revision": "e01ff66d3f1cbe034df5",
+    "url": "/joker/static/js/2.e01ff66d.chunk.js"
+  },
+  {
+    "revision": "d042a23ae94b92760225",
+    "url": "/joker/static/js/runtime~main.d042a23a.js"
+  },
+  {
+    "revision": "9f1193f101648a07c0869e44af81295c",
+    "url": "/joker/index.html"
+  }
+];
