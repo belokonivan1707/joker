065e1459133fa65c81eae80d29ddb2d4bf6c7d9e
ivan.bilokon <ivan.bilokon@jetsoftpro.com>
2022-12-23 12:53:11 +0200

    just for fun in my vocation

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -18,9 +18,11 @@ const App = () => {
   return (
     <div style={{padding: '10px'}}>
       <Banner words={['Привет, Олег!']}/>
-      <Banner words={['Надеюсь ты в отличном расположении духа!']} time={2500}/>
-      <Banner words={['И скоро перейдешь к следующему этапу!']} time={8000}/>
-      <Banner words={['Обнял и не приподнял!']} time={12000}/>
+      <Banner words={['Надеюсь ты в отличном']} time={2000}/>
+      <Banner words={['расположении духа!']} time={5000}/>
+      <Banner words={['И скоро перейдешь к']} time={8000}/>
+      <Banner words={['следующему этапу!']} time={11000}/>
+      <Banner words={['Обнял и не приподнял!']} time={15000}/>
     </div>
   );
 }
